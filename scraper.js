38f9b2edd7252feb4ab87258fe5d2a1e0e94564f
Leandro Outi Baroni <64474545+LiuDuty@users.noreply.github.com>
2026-02-19 10:22:59 -0300

    fix: adding puppeteer compatibility args for whatsapp connection

--- scraper.js
+++ scraper.js
@@ -54,7 +54,12 @@ const client = new Client({
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage'
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // Pode ajudar em containers com pouca RAM
+            '--disable-gpu'
         ]
     }
 });
