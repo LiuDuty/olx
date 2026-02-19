1c73d7400153c9c7627d7e07eb3265a36463ac49
Leandro Outi Baroni <64474545+LiuDuty@users.noreply.github.com>
2026-02-19 18:52:54 -0300

    chore: add master key warning log

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const MASTER_KEY = process.env.PARSE_MASTER_KEY; // Master key must be env var f
 const SERVER_URL = process.env.PARSE_SERVER_URL || 'https://parseapi.back4app.com/';
 
 console.log(`🛠️ Inicializando Parse com AppID: ${APP_ID.substring(0, 5)}...`);
+if (!MASTER_KEY) console.warn("⚠️ AVISO: PARSE_MASTER_KEY não configurada. Operações de MasterKey podem falhar.");
 
 Parse.initialize(APP_ID, JS_KEY, MASTER_KEY);
 Parse.serverURL = SERVER_URL;
